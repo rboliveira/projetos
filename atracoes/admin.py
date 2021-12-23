2c7282ee4c24e534738f606205718678729b558c
rboliveira.rs <Rodr#310b>
2021-12-23 20:11:31 -0300

    Initial commit

---
+++ atracoes/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from.models import Atracoes
+
+admin.site.register(Atracoes)
+
+
+
+
+
+# Register your models here.
+
---
+++ core/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from atracoes.models import Atracoes
+# Create your models here.
+
+class PontoTuristico(models.Model):
+    nome = models.CharField(max_length=150)
+    descricao = models.TextField()
+    aprovado = models.BooleanField(default=False)
+
+    atracoes = models.ManyToManyField(Atracoes)
+
+    def __str__(self):
+        return self.nome
+
